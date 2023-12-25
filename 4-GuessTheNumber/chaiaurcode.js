f50a00f698539f0fe997118866d7921a02e34b96
ShilpeshThakur <44371498+ShilpeshThakur@users.noreply.github.com>
2023-12-25 14:27:47 +0000

    guess code

---
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -0,0 +1,42 @@
+const randomNumber = parseInt(Math.random * 100 + 1);
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField').value
+const guessSlot = document.querySelector('.guesses').value
+const remaining = document.querySelector('.lastResult').value
+const lowOrHi = document.querySelector('.lowOrHi').value
+const startOver = document.querySelector('.resultParas').value
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+
+let playGmae = true;
+
+
+
+function validateGuess(guess){
+    //
+}
+
+function checkGuess(guess){
+    //
+}
+
+function displayGuess(guess){
+    //
+}
+
+function displayMessage(msg){
+    //
+}
+
+function endGame(){
+    //
+}
+
+function newGame(){
+    //
+}
+
